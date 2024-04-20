7df348838783838c5fb9bb3a760d6cdfea53a498
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-04-19 21:36:38 -0600

    [CREATE] ImplementOAUTH

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <b-card title="Card Title" no-body>
+    <b-card-header header-tag="nav">
+      <b-nav  class="box-shadow">
+        <b-nav-item to="/" >Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+        <b-nav-item v-if="$auth.isAuthenticated" to="/profile" >Profile</b-nav-item>
+      </b-nav>
+    </b-card-header>
+    <b-card-body class="text-center">
+      <router-view />
+    </b-card-body>
+  </b-card>
+    <!-- <div id="nav">
+      <router-link >Home</router-link>|
+      <router-link >About</router-link>|
+      <router-link >Profile</router-link>
+    </div> -->
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+.box-shadow {
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <b-row>
+      <b-col cols="12" sm="12" md="12">
+    <b-card class="mb-2 text-center">
+      <b-card-text>
+        <img 
+        :src="$auth.user.picture"
+        class="rounded-circle"
+        style="width: 200px; height: 200px; object-fit: cover;"
+        alt="Profile Image"
+      >
+        <h2>{{ $auth.user.name }}</h2>
+        <p>{{ $auth.user.email }}</p>
+      </b-card-text>
+
+      <b-card>
+        <b-card-text>
+          <div>
+            <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
+          </div>
+        </b-card-text>
+      </b-card>
+    </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
